21d623949fea60a4012c227d46d5ea65e7b1fdbf
Alex Bordun <116550793+alex-deity@users.noreply.github.com>
2025-07-10 22:00:41 +0200

    Fix translation loading and update fallback strategy in dynamic page

--- src/pages/dynamic/[...slug].tsx
+++ src/pages/dynamic/[...slug].tsx
@@ -24,7 +24,7 @@ export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
 
   return {
     props: {
-      ...(await serverSideTranslations(locale ?? 'pl-PL', ['common'])),
+      ...(await serverSideTranslations(locale ?? "pl-PL", ["common"])),
       foo: "bar",
       slug,
     },
@@ -35,6 +35,6 @@ export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
 export async function getStaticPaths() {
   return {
     paths: [],
-    fallback: "blocking",
+    fallback: true,
   };
 }
