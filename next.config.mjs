19e1d3009c870c258d3171efb4e5a6b0fa00a824
Alex Bordun <116550793+alex-deity@users.noreply.github.com>
2025-07-10 21:53:30 +0200

    Fix import statement and update i18n property access for correct configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-import { i18n } from "./next-i18next.config.js";
+import i18n from "./next-i18next.config.js";
 
 export default {
   i18n: {
-    defaultLocale: i18n.defaultLocale,
-    locales: i18n.locales,
-    localeDetection: i18n.localeDetection,
+    defaultLocale: i18n.i18n.defaultLocale,
+    locales: i18n.i18n.locales,
+    localeDetection: i18n.i18n.localeDetection,
   },
   basePath: "/pl",
   reactStrictMode: true,
