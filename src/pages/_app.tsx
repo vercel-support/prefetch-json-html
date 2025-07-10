a2d237e855e58790104b3e95a8bedd6ea6cfca3a
Alex Bordun <116550793+alex-deity@users.noreply.github.com>
2025-07-10 23:57:45 +0200

    Refactor i18n configuration to use emptyInitialI18NextConfig for improved clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { appWithTranslation } from "next-i18next";
+import { appWithTranslation, UserConfig } from "next-i18next";
 import nextI18NextConfig from "../../next-i18next.config.js";
+
+const emptyInitialI18NextConfig: UserConfig = {
+  i18n: {
+    defaultLocale: nextI18NextConfig.i18n.defaultLocale,
+    locales: nextI18NextConfig.i18n.locales,
+  },
+};
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(App, nextI18NextConfig);
+export default appWithTranslation(App, emptyInitialI18NextConfig);
